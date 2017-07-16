a85a7c76007e51fd8cb0d0f5e1b3bdcd9cd3face
Ueda Takeyuki <ueda@latelierdueda.com>
2017-07-16 11:41:32 +0900

    first commit

---
+++ regminion.sh
@@ -0,0 +1,5 @@
+serial_id=`python -m piserialnumber`
+minion_id=`cat /etc/salt/minion | grep -e "^id:" | cut -d' ' -f2`
+echo serial_id: $serial_id
+echo minion_id: $minion_id
+curl -F "serial_id=${serial_id}" -F "minion_id=${minion_id}" -F "command=register" http://lohengrin.uedasoft.com/monitor2/saltminion.php
