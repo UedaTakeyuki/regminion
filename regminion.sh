d3addf242be1ddd0ec7882918b36bb889384d6c1
Ueda Takeyuki <ueda@latelierdueda.com>
2017-07-16 11:51:10 +0900

    put url as command line param

--- regminion.sh
+++ regminion.sh
@@ -1,6 +1,8 @@
 # pip install piserialnumber
+url=$1 
 serial_id=`python -m piserialnumber`
 minion_id=`cat /etc/salt/minion | grep -e "^id:" | cut -d' ' -f2`
+echo url: $url
 echo serial_id: $serial_id
 echo minion_id: $minion_id
-curl -F "serial_id=${serial_id}" -F "minion_id=${minion_id}" -F "command=register" http://lohengrin.uedasoft.com/monitor2/saltminion.php
+curl -F "serial_id=${serial_id}" -F "minion_id=${minion_id}" -F "command=register" ${url}/saltminion.php
